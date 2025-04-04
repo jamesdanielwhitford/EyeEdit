2377589bde4c5a3ed5e14b111cef6bf6ac2aacf0
jamesdanielwhitford <james.whitford0@gmail.com>
2025-04-04 10:45:41 +0200

    Header intial Setup and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Header />
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
