260d894924b7ce57eb64f99231c0bda49bd93628
jamesdanielwhitford <james.whitford0@gmail.com>
2025-04-04 15:58:21 +0200

    Initial Project Configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default [
+  ...compat.config({
+    extends: ['eslint:recommended', 'next/core-web-vitals', 'plugin:prettier/recommended'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    },
+  }),
+];
